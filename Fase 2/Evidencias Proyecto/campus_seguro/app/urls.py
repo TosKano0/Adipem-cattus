2305d1131b64ae7a76e26f8b1d37c51a97a4ae37
TosKano0 <69978313+TosKano0@users.noreply.github.com>
2025-10-08 21:30:26 -0300

    urls.py

--- Fase 2/Evidencias Proyecto/campus_seguro/app/urls.py
+++ Fase 2/Evidencias Proyecto/campus_seguro/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, formulario_reporte
+from .views import home, usuario_principal, formulario_reporte
 
 urlpatterns = [
     path('', home, name="home"),
     path('formulario-reporte/', formulario_reporte, name="formulario-reporte"),
+    path('usuario_principal/', usuario_principal, name="usuario_principal"),
 ]
