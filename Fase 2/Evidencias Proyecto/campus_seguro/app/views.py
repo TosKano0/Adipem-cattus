f44c197a608e6a926abc0217a8c54007ee58042a
TosKano0 <69978313+TosKano0@users.noreply.github.com>
2025-10-08 21:28:17 -0300

    views.py

--- Fase 2/Evidencias Proyecto/campus_seguro/app/views.py
+++ Fase 2/Evidencias Proyecto/campus_seguro/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import ReporteForm, RegistroUsuarioForm  
+from .forms import ReporteForm, RegistroUsuarioForm
+from .models import RegistroUsuario
 
 # Create your views here.
 def formulario_reporte(request):
@@ -9,7 +10,7 @@ def formulario_reporte(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Reporte creado con éxito.")
-            return redirect("home")  # o a una lista/detalle
+            return redirect("usuario_principal")  # o a una lista/detalle
         else:
             messages.error(request, "Revisa los errores del formulario.")
     else:
@@ -21,11 +22,16 @@ def home(request):
         form = RegistroUsuarioForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "✅ Usuario registrado correctamente.")
-            return redirect("home")
+            messages.success(request, "Usuario registrado correctamente.")
+            return redirect("usuario_principal")
         else:
-            messages.error(request, "⚠️ Revisa los campos marcados.")
+            messages.error(request, "Revisa los campos marcados.")
     else:
         form = RegistroUsuarioForm()
 
     return render(request, "app/home.html", {"form": form})
+
+def usuario_principal(request):
+    # Obtener el último usuario registrado
+    usuario = RegistroUsuario.objects.last()
+    return render(request, "app/usuario_principal.html", {"usuario": usuario})
