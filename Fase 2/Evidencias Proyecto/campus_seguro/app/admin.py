99d7d22c7586470fd896920b45cc7864b829bc85
TosKano0 <69978313+TosKano0@users.noreply.github.com>
2025-10-08 21:36:29 -0300

    admin.py

--- Fase 2/Evidencias Proyecto/campus_seguro/app/admin.py
+++ Fase 2/Evidencias Proyecto/campus_seguro/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from app.models import Reporte
+from app.models import RegistroUsuario
 
 # Register your models here.
 admin.site.register(Reporte)
+admin.site.register(RegistroUsuario)
